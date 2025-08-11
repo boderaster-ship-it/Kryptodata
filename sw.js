0544dacf84a9df94f67727e730158b2a1a16e7db
boderaster-ship-it <bode.raster@icloud.com>
2025-08-12 00:52:20 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,19 @@
-const CACHE = 'static-v1';
+const CACHE = 'static-v2'; // Version bump, damit alte Dateien verworfen werden
 const ASSETS = [
   './','./index.html','./style.css','./app.js','./providers.js','./storage.js','./export.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'
 ];
+
 self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
+
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', e=>{
   const url = new URL(e.request.url);
   if (ASSETS.includes(url.pathname) || url.origin===location.origin){
